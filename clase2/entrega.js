0baa074913ca38b15fe3d86df4188df1f080375e
EzequielUS <ezequiel.ustar@gmail.com>
2024-01-30 23:43:28 -0300

    getProductById implementation

--- clase2/entrega.js
+++ clase2/entrega.js
@@ -8,6 +8,22 @@ class ProductManager {
     addProduct(product) {
         // Reviso que cuente con todos los campos necesarios
         let fields = Object.keys(product)
-
     }
+
+    getProducts() {
+        return this.products
+    }
+
+    getProductById(id){
+        if (this.products.includes(id)) {
+            console.log(this.products)
+        } else {
+            console.log('Not found')
+        }
+    }
+
 }
+
+// Testing
+// const manager = new ProductManager();
+// let currentProducts = manager.getProducts()
