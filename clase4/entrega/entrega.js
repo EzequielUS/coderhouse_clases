537d24f4dab39a08a40aff6e26f0c455bb57f7d7
EzequielUS <ezequiel.ustar@gmail.com>
2024-02-06 19:55:56 -0300

    ejercicio entregable clase 4

---
+++ clase4/entrega/entrega.js
@@ -0,0 +1,101 @@
+const fs = require('fs')
+
+class ProductManager {
+    constructor () {
+        this.products = []
+        this.path = './productos.json'
+    }
+
+    static productIdCounter = 1;
+
+    addProduct(product) {
+        if (!product.title || !product.description || !product.price || !product.thumbnail || !product.code || !product.stock) {
+            console.log("Error: Todos los campos son obligatorios.");
+            return;
+        }
+
+        if (this.products.some(prod => prod.code === product.code)){
+            console.log("Error: El código ingresado ya existe en otro producto.");
+            return;
+        }
+
+        product.id = ProductManager.productIdCounter;
+        ProductManager.productIdCounter += 1;
+        
+        let jsonProduct = JSON.stringify(product);
+        if (!fs.existsSync(this.path)) {
+            fs.writeFileSync(this.path, jsonProduct);
+        }
+        else {
+            fs.appendFileSync(this.path, jsonProduct);
+        }
+
+    }
+
+    getProducts() {
+        let products = []
+        if (!fs.existsSync(this.path)) {
+            products = fs.readFileSync(this.path);
+            products = JSON.parse(products);
+        }
+
+        return products
+    }
+
+    getProductById(id){
+        let product = this.products.find(prod => prod.id === id)
+
+        if (!product) {
+            console.log('Not found');
+        }
+
+        return product
+    }
+
+    updateProduct(id){
+
+    }
+
+    deleteProduct(id){
+        
+    }
+}
+
+let product1 = {
+    'title': 'test',
+    'description': 'test',
+    'price': 100,
+    'thumbnail': 'test.png',
+    'code': 1,
+    'stock': 100,
+}
+
+let product2 = {
+    'title': 'test',
+    'description': 'test',
+    'price': 100,
+    'thumbnail': 'test.png',
+    'code': 2,
+    'stock': 100,
+}
+
+// Testing
+const manager = new ProductManager();
+
+// get current products
+let currentProducts = manager.getProducts();
+console.log('Productos actuales:', currentProducts);
+
+// // add new product
+// manager.addProduct(product1);
+// currentProducts = manager.getProducts();
+// console.log('Productos actuales:', currentProducts);
+
+// manager.addProduct(product2);
+// currentProducts = manager.getProducts();
+// console.log('Productos actuales:', currentProducts);
+
+// // get product by id
+// let id = 2;
+// let product = manager.getProductById(id);
+// console.log(`El producto con el id ${id}, es: `, product);
